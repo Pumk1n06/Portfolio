1cc5e6c30a90ae56c6d9ef7c93bf7381eee0dc9c
Pumk1n06 <poojithukaradi06@gmail.com>
2023-10-25 19:38:32 +0530

    Portfolio

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+import Car from '../assets/projects/workImg.jpeg'
+import Online from '../assets/projects/realestate.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"Online Examination System",
+        image:Online,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"Car Booking Management",
+        image:Car,
+        github:"",
+        live:"",
+    },
+    
+
+
+]
